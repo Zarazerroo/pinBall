48b18f5700303b14da1713503f81ffe16d7be433
Alonizi <Abdulaziz.Alonizi@gmail.com>
2025-01-12 13:44:18 +0300

    change level score logic

--- Assets/Scripts/Aziz/LevelController.cs
+++ Assets/Scripts/Aziz/LevelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -9,18 +10,24 @@ public class LevelController : MonoBehaviour
     [SerializeField] private Text LivesText;
     [SerializeField] private GameObject ballPrefab;
     public Vector3 ballRespawnPosition; 
-    public int lives = 3;
+    //public int lives = 3;
+    public ScoreKeeper score; 
+    public int ballsCount = 1 ; 
     public void RespawnBall()
     {
+        score = FindAnyObjectByType<ScoreKeeper>();
         Instantiate(ballPrefab, ballRespawnPosition, quaternion.identity);
     }
     
     public void DestroyBall(GameObject ball)
     {
+        
+        ballsCount--;
         Destroy(ball);
-        lives--;
-        LivesText.text = lives.ToString();
-        if(lives> 0 )
-            RespawnBall();
+        //lives--;
+        //LivesText.text = lives.ToString();
+        if (ballsCount < 0)
+            score.ResetScore();
+            
     }
 }
