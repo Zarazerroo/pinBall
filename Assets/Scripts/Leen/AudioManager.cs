a964d311ea8d25ce554f8d8edc5c12a05995e708
iLeenx <LeenALshehrii@hotmail.com>
2025-01-19 09:49:23 +0300

    Sound1

--- Assets/Scripts/Leen/AudioManager.cs
+++ Assets/Scripts/Leen/AudioManager.cs
@@ -5,12 +5,21 @@ public class AudioManager : MonoBehaviour
     [Header("---Audio Source---")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
-    [SerializeField] AudioSource WalkSource;
+    
 
     [Header("---Audio Clip---")]
     public AudioClip background;
     public AudioClip cuphit;
     public AudioClip pothit;
+
+    [Header("---New Audio Clip---")]
+    public AudioClip spring;
+    public AudioClip flippers;
+    public AudioClip blueitem;
+    public AudioClip purpleitem;
+    public AudioClip honey;
+    public AudioClip boosters;
+    public AudioClip shredding;
     
 
     void Start()
@@ -19,4 +28,9 @@ public class AudioManager : MonoBehaviour
         musicSource.clip = background;
         musicSource.Play();
     }  
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 }
