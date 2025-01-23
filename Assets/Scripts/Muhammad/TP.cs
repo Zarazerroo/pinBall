71e76541b2644f529c1b00f60741cb2c7d8c9011
muhammad Alhasan <145725473+elminato@users.noreply.github.com>
2025-01-23 10:29:37 +0300

    tp logic with the lights

--- Assets/Scripts/Muhammad/TP.cs
+++ Assets/Scripts/Muhammad/TP.cs
@@ -1,17 +1,46 @@
 // Copyright 2025 Muhammad Alhasan. All rights reserved.
 
+using NUnit.Framework;
+using System.Collections;
 using UnityEngine;
 public class TP : MonoBehaviour
 {
 
 	public Vector2 TP_end = new Vector2();
+	GameObject[] list;
 
-	void OnTriggerEnter2D(Collider2D other)
+	void Start()
 	{
-		if (other.gameObject.CompareTag("Ball"))
-		{
-			other.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
-			other.transform.position = TP_end;
-		}
+		list = GameObject.FindGameObjectsWithTag("Light");
 	}
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.gameObject.CompareTag("Ball"))
+      {
+         // Stop the ball's movement and teleport it
+         other.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+         other.transform.position = TP_end;
+
+         // Turn off all lights
+         foreach (GameObject light in list)
+         {
+            light.SetActive(false);
+         }
+
+         // Start the coroutine to turn lights back on
+         StartCoroutine(WaitAndTurnLightsOn());
+      }
+   }
+
+   IEnumerator WaitAndTurnLightsOn()
+   {
+      yield return new WaitForSeconds(2); // Wait for 2 seconds
+
+      // Turn all lights back on
+      foreach (GameObject light in list)
+      {
+         light.SetActive(true);
+      }
+   }
 }
