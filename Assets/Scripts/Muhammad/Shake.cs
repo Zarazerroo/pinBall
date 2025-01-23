0428bca1e1d853cdd98c70a6ab330cf919e760e9
Zarazerroo <158998754+Zarazerroo@users.noreply.github.com>
2025-01-23 16:24:52 +0300

    hi

--- Assets/Scripts/Muhammad/Shake.cs
+++ Assets/Scripts/Muhammad/Shake.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Shake : MonoBehaviour
 {
+	public Animator curtinsHolderAnimator;
+
+
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.CompareTag("Ball"))
 		{
 			Debug.Log("Shake");
-			// you can put the animation trigger here.
+
+			curtinsHolderAnimator.SetTrigger("Shake");
+
 		}
 	}
 }
